93077ab35c0764ca4824791376a4019b35b3d97f
Caijinglong <cjl_spy@163.com>
2018-10-15 15:12:23 +0800

    chinese local

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,41 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:localization_error_example/chinese_localization.dart';
 
 void main() => runApp(new MyApp());
 
 Iterable<LocalizationsDelegate<dynamic>> localDelegates = <LocalizationsDelegate<dynamic>>[
   // ... app-specific localization delegate[s] here
-  DefaultCupertinoLocalizations.delegate,
+  ChineseCupertinoLocalizations.delegate,
+  // DefaultCupertinoLocalizations.delegate,
   GlobalMaterialLocalizations.delegate,
   GlobalWidgetsLocalizations.delegate,
+];
+
+var locale = <Locale>[
+  const Locale('en'), // English
+  const Locale('zh'), // China
+  // ... other locales the app supports
 ];
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-      title: 'Flutter Demo',
-      theme: new ThemeData(
-        primarySwatch: Colors.blue,
-        platform: TargetPlatform.iOS,
+    return Localizations(
+      child: new MaterialApp(
+        title: 'Flutter Demo',
+        theme: new ThemeData(
+          primarySwatch: Colors.blue,
+          platform: TargetPlatform.iOS,
+        ),
+        home: TextFieldExample(),
+        localizationsDelegates: localDelegates,
+        supportedLocales: locale,
       ),
-      home: TextFieldExample(),
-      localizationsDelegates: localDelegates,
-      supportedLocales: <Locale>[
-        const Locale('en'), // English
-        const Locale('zh'), // China
-        // ... other locales the app supports
-      ].toList(),
+      delegates: localDelegates,
+      locale: const Locale('zh'),
     );
   }
 }
